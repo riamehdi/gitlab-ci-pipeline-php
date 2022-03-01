f73f132c73dd8bd82584b673073be9612bb89fb9
KHADIR <m@khadir.net>
2022-03-01 17:50:49 +0100

    ci: build php:7.4

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker pull php:7.4-alpine || true
-docker pull riamehdi/gitlab-ci-pipeline-php:7.4-alpine || true
-docker build --compress --cache-from riamehdi/gitlab-ci-pipeline-php:7.4-alpine --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t riamehdi/gitlab-ci-pipeline-php:7.4-alpine -f php/7.4/alpine/Dockerfile-lts .
-docker run -t --rm -v "$(pwd)":/var/www/html riamehdi/gitlab-ci-pipeline-php:7.4-alpine goss -g tests/goss-lts.yaml v
-docker tag riamehdi/gitlab-ci-pipeline-php:7.4-alpine riamehdi/gitlab-ci-pipeline-php:7.4-alpine
-docker push riamehdi/gitlab-ci-pipeline-php:7.4-alpine
+docker pull php:7.4 || true
+docker pull riamehdi/gitlab-ci-pipeline-php:7.4 || true
+docker build --compress --cache-from riamehdi/gitlab-ci-pipeline-php:7.4 --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t riamehdi/gitlab-ci-pipeline-php:7.4 -f php/7.4/Dockerfile .
+docker run -t --rm -v "$(pwd)":/var/www/html riamehdi/gitlab-ci-pipeline-php:7.4 goss -g tests/goss-7.4.yaml v
+docker tag riamehdi/gitlab-ci-pipeline-php:7.4 riamehdi/gitlab-ci-pipeline-php:7.4
+docker push riamehdi/gitlab-ci-pipeline-php:7.4
